2f51d2e75e0ef7b75d5ccad67c778de1b4abcbae
wmshort <w.short@exeter.ac.uk>
2019-10-31 15:05:15 +0000

    add caching

--- latinwordnet/latinwordnet.py
+++ latinwordnet/latinwordnet.py
@@ -1,7 +1,7 @@
 """A light-weight wrapper for the Latin WordNet API"""
 
 import requests
-
+from functools import lru_cache
 
 class Semfields:
     def __init__(self, host, code=None, english=None, token=None):
@@ -223,12 +223,14 @@ class LatinWordNet:
         self.host = host.rstrip("/")
         self.token = token
 
+    @lru_cache
     def lemmatize(self, form: str, pos: str = None):
         results = requests.get(
             f"{self.host}/lemmatize/{form}/{f'{pos}/' if pos else ''}?format=json", headers=self.get_auth()
         )
         return iter(results.json()) if results else []
 
+    @lru_cache
     def translate(self, language: str, form: str, pos: str = "*"):
         pos = f"{pos}/" if pos else ""
         results = requests.get(
@@ -236,6 +238,7 @@ class LatinWordNet:
         )
         return iter(results.json()) if results else []
 
+    @lru_cache
     def sentiment_analysis(self, text, weighting=None, excluded=None):
         """
         :param text: The string to be analyzed.
@@ -254,18 +257,23 @@ class LatinWordNet:
         results = requests.post(f"{self.host}/sentiment/", data=data, headers=self.get_auth(), verify=True)
         return results
 
+    @lru_cache
     def lemmas(self, lemma=None, pos=None, morpho=None):
         return Lemmas(self.host, lemma, pos, morpho)
 
+    @lru_cache
     def lemmas_by_uri(self, uri):
         return Lemmas(self.host, uri=uri)
 
+    @lru_cache
     def synsets(self, pos: str = None, offset: str = None, gloss: str = None):
         return Synsets(self.host, pos, offset, gloss)
 
+    @lru_cache
     def semfields(self, code: str = None, english: str = None):
         return Semfields(self.host, code, english)
 
+    @lru_cache
     def index(self, pos=None, morpho=None):
         pos = f"{pos}/" if pos else "*/"
         morpho = f"{morpho}/" if morpho else ""
